c40ed78305ed7733c3b666657fb3037e9deb48b8
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-10-13 12:09:19 +0100

    Code refactoring and addition of api endpoints

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const authRouter = require("./routes/auth")
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+const accountsRouter = require("./routes/accounts");
+const transactionsRouter = require("./routes/transactions")
+
+const isAuth = require("./auth/isauth");
 
 var app = express();
 
@@ -13,29 +17,30 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/v1', indexRouter);
-app.use('/api/v1/users', usersRouter);
+app.use("/api/v1", indexRouter);
+app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/login", authRouter);
+app.use("/api/v1/accounts", isAuth, accountsRouter);
+app.use("/api/v1/transactions", isAuth, transactionsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  .send({error: err.message})
+  res.status(err.status || 500).send({ error: err.message });
 });
 
 module.exports = app;
