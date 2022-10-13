700572d68dc693623e053817ef6dcea73e3c970c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-10-13 12:27:40 +0100

    Update trasactions migration
    The changes to the table are:
    - drops `user_id` foreign key
    - renames `user_id` to `account_id`
    - creates `account_id` foreign key

---
+++ knex/migrations/20221013064515_update_transactions.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("transactions", (table) => {
+    table.dropForeign("user_id");
+    table.renameColumn("user_id", "account_id");
+    table.foreign("account_id").references("id").inTable("accounts");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("transactions", (table) => {
+    table.dropForeign("accoun_id");
+    table.renameColumn("account_id", "user_id");
+    table.foreign("user_id").references("id").inTable("users");
+  });
+};
