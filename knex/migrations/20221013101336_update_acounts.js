b3ea4c308c4a79a97679eb5bb91eeadd59849dbe
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-10-15 11:28:53 +0100

    Knex rollback error fix
    Cannot drop the unique column as it is a foreign key.
    First drop the foreign key and then drop the unique column.
    Finally create the foreign key

--- knex/migrations/20221013101336_update_acounts.js
+++ knex/migrations/20221013101336_update_acounts.js
@@ -2,18 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.table("accounts", table => {
-    table.unique(["user_id"])
-  })
+exports.up = function (knex) {
+  return knex.schema.table("accounts", (table) => {
+    table.unique(["user_id"]);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.table("accounts", table => {
-        table.dropUnique(["user_id"])
-      })
+exports.down = function (knex) {
+  return knex.schema.table("accounts", (table) => {
+    table.dropForeign("user_id");
+    table.dropUnique(["user_id"]);
+    table.foreign("user_id").references("id").inTable("users");
+  });
 };
