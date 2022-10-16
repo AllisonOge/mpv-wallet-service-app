15e4eab7e30dde53ac0db2b9805ada78a76c5ebf
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-10-16 17:37:56 +0100

    Implementation of users test script
    Checks for fails and successes.
    Fails if email already exist and success otherwise.

---
+++ test/users.js
@@ -0,0 +1,75 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const database = require("../knex/database");
+const should = chai.should();
+
+const app = require("../app");
+
+chai.use(chaiHttp);
+
+describe("Users", () => {
+  beforeEach((done) => {
+    // clear the database
+    // rollback and
+    // update migrations
+    database
+      .delete()
+      .from("transactions")
+      .then((_) => database.delete().from("accounts"))
+      .then((_) => database.delete().from("users"))
+      .then((_) => database.migrate.rollback([], [true]))
+      .then((_) => database.migrate.latest())
+      .then((_) => {
+        console.log("DONE");
+        done();
+      });
+  });
+
+  describe("POST users", () => {
+    // create a new user
+    it("It should create a new user", (done) => {
+      const newUser = {
+        username: "ohgay@justohgay.com",
+        password: "paSSword_123",
+      };
+
+      chai
+        .request(app)
+        .post("/api/v1/users")
+        .send(newUser)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+          res.body.should.have.property("id");
+          res.body.should.have.property("username");
+          done();
+        });
+    });
+
+    // raise an exception
+    it("It should fail because the username already exist", (done) => {
+      const newUser = {
+        username: "ohgay@justohgay.com",
+        password: "paSSword_123",
+      };
+
+      chai.request(app).post("/api/v1/users").send(newUser).end();
+
+      chai
+        .request(app)
+        .post("/api/v1/users")
+        .send(newUser)
+        .end((err, res) => {
+          res.should.have.status(409);
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("details")
+            .equal(`Duplicate entry: ${newUser.username} already exist`);
+
+          // close connection pool
+          database.destroy();
+          done();
+        });
+    });
+  });
+});
