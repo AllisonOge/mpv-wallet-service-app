3c86af881fd7b026370184cab944e4378138286a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-10-16 20:22:50 +0100

    Enabling CORS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
-const cors = require("cors")
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -22,8 +22,30 @@ var app = express();
 // app.set('view engine', 'jade');
 
 // app.use(logger("dev"));
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Contorl-Allow-Methods",
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "PATCH",
+    "HEAD",
+    "OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept"
+  );
+  return next()
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
